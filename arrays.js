9d15ffd098dc0a151fa2647c75cb2737397718e6
Dennis Lord <lorddennis1006@gmail.com>
2018-03-26 17:58:56 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,3 +1,3 @@
 var chocolateBars = ['snickers', 'hundred grand','kitkat', 'skittles']
-function addElementToBeginningOfArray(array, element){return ['element',...'array']}
-function desctructivelyAddElementToBeginningOfArray(array, element){'array'.unshift('elemen't);return 'array'}
+function addElementToBeginningOfArray(array, element){return [element,...array]}
+function desctructivelyAddElementToBeginningOfArray(array, element){array'.unshift(element);returnarray}
