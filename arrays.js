2bc08a17283080335f3e8b85aa887edf40b2bff4
Dennis Lord <lorddennis1006@gmail.com>
2018-03-26 17:55:31 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,2 +1,3 @@
-function addElementToBeginningOfArray(array, element){return [candyString,...chocolateBars]}
+var chocolateBars = ['snickers', 'hundred grand','kitkat', 'skittles']
+function addElementToBeginningOfArray(array, element){return [element,...array]}
 function desctructivelyAddElementToBeginningOfArray(array, element){array.unshift(element);return array}
