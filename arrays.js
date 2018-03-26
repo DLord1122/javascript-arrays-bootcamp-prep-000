bce0076a0c375e9be71e24a4e6490bb7817041d3
Dennis Lord <lorddennis1006@gmail.com>
2018-03-26 17:47:58 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,2 +1,2 @@
 function addElementToBeginningOfArray(chocolateBars, candyString){return [candyString,...chocolateBars]}
-function desctructivelyAddElementToBeginningOfArray(lolipop,popsickle)
+function desctructivelyAddElementToBeginningOfArray(lolipop,popsickle){lolipop.unshift(popsickle)}
